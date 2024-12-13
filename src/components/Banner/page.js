2e0c9c8644e175214afda270a9c65f248c95de9d
Oleksandr <meduzko95@gmail.com>
2024-12-13 18:17:46 +0100

    Banner mobile image

--- src/components/Banner/page.js
+++ src/components/Banner/page.js
@@ -1,18 +1,22 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './styles.module.css';
+import { getDeviceType } from '../../helpers';
 
 export default function Banner() {
+  const device = getDeviceType();
+  const isMobile = device !== 'desktop';
+
   return (
     <div className={styles.bannerContainer}>
       <Image
-        src="/banner.webp"
+        src={isMobile ? '/banner_mobile.webp' : '/banner.webp'}
         alt="Main banner"
         fill
-        objectFit="cover"
         priority={true}
         quality={35}
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 100vw"
+        sizes="100vw"
+        style={{ objectFit: 'cover' }}
         className={styles.bannerImg}
       />
       <h1 className={styles.bannerText}>Срібні прикраси Daisy Jewellery — ваш улюблений інтернет-магазин</h1>
